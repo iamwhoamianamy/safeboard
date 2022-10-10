3212606794a40b1ba5e342c6b60263b6f96a21e9
Arthur Efremov <anon.yu@mail.ru>
2022-10-10 22:03:27 +0300

    Implemented simple scanner

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ file_generator: file_generator.cpp
 	g++ file_generator.cpp -o file_generator
 file_contaminator: file_contaminator.cpp
 	g++ file_contaminator.cpp -o file_contaminator
+scanner: scanner.cpp
+	g++ scanner.cpp -o scanner
---
+++ scanner.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+const std::string jsSus = "<script>evil_script()</script>";
+const std::string unixSus = "rm -rf ~/Documents";
+const std::string macOSSus = "system(\"launchctl load /Library/LaunchAgents/com.malware.agent\")";
+
+enum class SusType
+{
+    None,
+    Js,
+    Unix,
+    MacOS,
+    Error
+};
+
+SusType checkForSuspicion(const std::filesystem::path& path)
+{
+    std::ifstream fin;
+
+    try
+    {
+        fin.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+        fin.open(path, std::ios::in);
+
+        std::string line;
+
+        while (std::getline(fin, line))
+        {
+            if (path.string().find(".js") != std::string::npos)
+            {
+                if (line.find(jsSus) != std::string::npos)
+                {
+                    return SusType::Js;
+                }
+            }
+            else
+            {
+                if (line.find(unixSus) != std::string::npos)
+                {
+                    return SusType::Unix;
+                }
+                else if (line.find(macOSSus) != std::string::npos)
+                {
+                    return SusType::MacOS;
+                }
+            }
+        }        
+    }
+    catch(const std::exception& e)
+    {
+        if(!fin.eof())
+        {
+            return SusType::Error;
+        }
+    }
+
+    return SusType::None;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Not enough params! Example:" << std::endl;
+        std::cout << argv[0] << " " << "/directory" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        std::filesystem::path path(argv[1]);
+        
+        size_t jsFiles = 0;
+        size_t unixFiles = 0;
+        size_t macOSFiles = 0; 
+        size_t errors = 0;
+        size_t proccessed = 0;
+
+        auto start = std::chrono::steady_clock::now();
+
+        for (const auto& file : std::filesystem::directory_iterator(path))
+        {
+            SusType sus = checkForSuspicion(file.path());
+
+            switch (sus)
+            {
+                case SusType::Js: jsFiles++; break;
+                case SusType::Unix: unixFiles++; break;
+                case SusType::MacOS: macOSFiles++; break;
+                case SusType::Error: errors++; break;
+                default: break;
+            }
+
+            proccessed++;
+        }
+
+        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() / 1000.0;
+
+        std::cout << "====== Scan result ======" << std::endl;
+        std::cout << "Processed files: " << proccessed << std::endl;
+        std::cout << "JS detects: " << jsFiles << std::endl;
+        std::cout << "Unix detects: " << unixFiles << std::endl;
+        std::cout << "macOS detects: " << macOSFiles << std::endl;
+        std::cout << "Errors: " << errors << std::endl;
+        std::cout << "Exection time: " << time << std::endl;
+        std::cout << "=========================" << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+    return 0;
+}
