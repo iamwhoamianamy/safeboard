db393daef84a81d3b3cb8303141b326c665961c7
Arthur Efremov <anon.yu@mail.ru>
2022-10-12 19:49:41 +0300

    Added sources for client and server

---
+++ server.cpp
@@ -0,0 +1,412 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/config.hpp>
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace beast = boost::beast;   // from <boost/beast.hpp>
+namespace http = beast::http;     // from <boost/beast/http.hpp>
+namespace net = boost::asio;      // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
+
+enum class SusType
+{
+    None,
+    Js,
+    Unix,
+    MacOS,
+    Error
+};
+
+SusType checkForSuspicion(const std::filesystem::path& path)
+{
+    std::ifstream fin;
+    fin.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        fin.open(path, std::ios::in);
+
+        std::string line;
+
+        while (std::getline(fin, line))
+        {
+            if (path.string().find(".js") != std::string::npos)
+            {
+                if (line.find(jsSus) != std::string::npos)
+                {
+                    return SusType::Js;
+                }
+            }
+            else
+            {
+                if (line.find(unixSus) != std::string::npos)
+                {
+                    return SusType::Unix;
+                }
+                else if (line.find(macOSSus) != std::string::npos)
+                {
+                    return SusType::MacOS;
+                }
+            }
+        }        
+    }
+    catch(const std::exception& e)
+    {
+        if(!fin.eof())
+        {
+            return SusType::Error;
+        }
+    }
+
+    return SusType::None;
+}
+
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template <class Body, class Allocator, class Send>
+void handle_request(
+    beast::string_view doc_root,
+    http::request<Body, http::basic_fields<Allocator>> &&req,
+    Send &&send)
+{
+    // Returns a bad request response
+    auto const bad_request =
+        [&req](beast::string_view why)
+    {
+        http::response<http::string_body> res{http::status::bad_request, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = std::string(why);
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a not found response
+    auto const not_found =
+        [&req](beast::string_view target)
+    {
+        http::response<http::string_body> res{http::status::not_found, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "The resource '" + std::string(target) + "' was not found.";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a server error response
+    auto const server_error =
+        [&req](beast::string_view what)
+    {
+        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "An error occurred: '" + std::string(what) + "'";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Make sure we can handle the method
+    if (req.method() != http::verb::get)
+    {
+        return send(bad_request("Unknown HTTP-method"));
+    }
+
+    // Request path must be absolute and not contain "..".
+    if (req.target().empty() ||
+        req.target()[0] != '/' ||
+        req.target().find("..") != beast::string_view::npos)
+    {
+        return send(bad_request("Illegal request-target"));
+    }
+
+    std::string path(req.target());
+    ScanResults scanResults;        
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (const auto& file : std::filesystem::directory_iterator(path))
+    {
+        SusType sus = checkForSuspicion(file.path());
+        scanResults.add(sus);
+    }
+    
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() / 1000.0;
+
+    // Handle the case where the file doesn't exist
+    if (ec == beast::errc::no_such_file_or_directory)
+        return send(not_found(req.target()));
+
+    // Handle an unknown error
+    if (ec)
+        return send(server_error(ec.message()));
+
+    // Cache the size since we need it after the move
+    auto const size = body.size();
+
+    // Respond to GET request
+    http::response<http::file_body> res{
+        std::piecewise_construct,
+        std::make_tuple(std::move(body)),
+        std::make_tuple(http::status::ok, req.version())};
+
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, mime_type(path));
+    res.content_length(size);
+    res.keep_alive(req.keep_alive());
+    return send(std::move(res));
+}
+
+//------------------------------------------------------------------------------
+
+// Report a failure
+void fail(beast::error_code ec, char const *what)
+{
+    std::cout << what << ": " << ec.message() << "\n";
+}
+
+// Handles an HTTP server connection
+class session : public std::enable_shared_from_this<session>
+{
+    // The function object is used to send an HTTP message.
+    struct send_lambda
+    {
+        session &self_;
+
+        explicit send_lambda(session &self) : self_(self) { }
+
+        template <bool isRequest, class Body, class Fields>
+        void operator()(http::message<isRequest, Body, Fields> &&msg) const
+        {
+            // The lifetime of the message has to extend
+            // for the duration of the async operation so
+            // we use a shared_ptr to manage it.
+            auto sp = std::make_shared<http::message<isRequest, Body, Fields>>(std::move(msg));
+            self_.res_ = sp;
+
+            // Write the response
+            http::async_write(
+                self_.stream_,
+                *sp,
+                beast::bind_front_handler(
+                    &session::on_write,
+                    self_.shared_from_this(),
+                    sp->need_eof()));
+        }
+    };
+
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_;
+    std::shared_ptr<std::string const> doc_root_;
+    http::request<http::string_body> req_;
+    std::shared_ptr<void> res_;
+    send_lambda lambda_;
+
+public:
+    // Take ownership of the stream
+    session(
+        tcp::socket &&socket,
+        std::shared_ptr<std::string const> const &doc_root)
+        : stream_(std::move(socket)), doc_root_(doc_root), lambda_(*this)
+    {
+    }
+
+    // Start the asynchronous operation
+    void run()
+    {
+        // We need to be executing within a strand to perform async operations
+        // on the I/O objects in this session. Although not strictly necessary
+        // for single-threaded contexts, this example code is written to be
+        // thread-safe by default.
+        net::dispatch(stream_.get_executor(),
+                      beast::bind_front_handler(
+                          &session::do_read,
+                          shared_from_this()));
+    }
+
+    void do_read()
+    {
+        // Make the request empty before reading,
+        // otherwise the operation behavior is undefined.
+        req_ = {};
+
+        // Set the timeout.
+        stream_.expires_after(std::chrono::seconds(30));
+
+        // Read a request
+        http::async_read(stream_, buffer_, req_,
+                         beast::bind_front_handler(
+                             &session::on_read,
+                             shared_from_this()));
+    }
+
+    void on_read(beast::error_code ec, std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        // This means they closed the connection
+        if (ec == http::error::end_of_stream)
+            return do_close();
+
+        if (ec)
+            return fail(ec, "read");
+
+        // Send the response
+        handle_request(*doc_root_, std::move(req_), lambda_);
+    }
+
+    void on_write( bool close, beast::error_code ec, size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if (ec)
+            return fail(ec, "write");
+
+        if (close)
+        {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            return do_close();
+        }
+
+        // We're done with the response so delete it
+        res_ = nullptr;
+
+        // Read another request
+        do_read();
+    }
+
+    void do_close()
+    {
+        // Send a TCP shutdown
+        beast::error_code ec;
+        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+
+        // At this point the connection is closed gracefully
+    }
+};
+
+//------------------------------------------------------------------------------
+
+// Accepts incoming connections and launches the sessions
+class listener : public std::enable_shared_from_this<listener>
+{
+    net::io_context &ioc_;
+    tcp::acceptor acceptor_;
+    std::shared_ptr<std::string const> doc_root_;
+
+public:
+    listener(
+        net::io_context &ioc,
+        tcp::endpoint endpoint,
+        std::shared_ptr<std::string const> const &doc_root) :
+            ioc_(ioc), acceptor_(net::make_strand(ioc)), doc_root_(doc_root)
+    {
+        beast::error_code ec;
+
+        // Open the acceptor
+        acceptor_.open(endpoint.protocol(), ec);
+        if (ec)
+        {
+            fail(ec, "open");
+            return;
+        }
+
+        // Allow address reuse
+        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        if (ec)
+        {
+            fail(ec, "set_option");
+            return;
+        }
+
+        // Bind to the server address
+        acceptor_.bind(endpoint, ec);
+        if (ec)
+        {
+            fail(ec, "bind");
+            return;
+        }
+
+        // Start listening for connections
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+
+        if (ec)
+        {
+            fail(ec, "listen");
+            return;
+        }
+    }
+
+    // Start accepting incoming connections
+    void run()
+    {
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(
+            net::make_strand(ioc_),
+            beast::bind_front_handler(
+                &listener::on_accept,
+                shared_from_this()));
+    }
+
+    void on_accept(beast::error_code ec, tcp::socket socket)
+    {
+        if (ec)
+        {
+            fail(ec, "accept");
+            return; // To avoid infinite loop
+        }
+        else
+        {
+            // Create the session and run it
+            std::make_shared<session>(std::move(socket), doc_root_)->run();
+        }
+
+        // Accept another connection
+        do_accept();
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    const auto address = net::ip::make_address(argv[1]);
+    const auto port = static_cast<unsigned short>(std::atoi(argv[2]));
+    const auto threads = 100;
+
+    net::io_context ioc{threads};
+    std::make_shared<listener>(ioc, tcp::endpoint{address, port}, doc_root)->run();
+
+    // Run the I/O service on the requested number of threads
+    std::vector<std::thread> v;
+    v.reserve(threads - 1);
+    for (auto i = threads - 1; i > 0; --i)
+    {
+        v.emplace_back(
+            [&ioc]
+            {
+                ioc.run();
+            });
+    }
+    ioc.run();
+
+    return EXIT_SUCCESS;
+}
