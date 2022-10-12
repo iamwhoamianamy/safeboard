ad17c6ca7a992ffc5f1f05c161b4af1425221f05
Arthur Efremov <anon.yu@mail.ru>
2022-10-12 22:38:19 +0300

    Creator script now generates multiple direcories

--- create_files.sh
+++ create_files.sh
@@ -4,6 +4,9 @@ dir=/home/arthur/many_dirs
 
 rm -r $dir
 mkdir $dir
-./file_generator 1000 $dir/many_files
-touch $dir/true_report.txt
-./file_contaminator $dir/many_files > $dir/true_report.txt
+
+for i in 1 2
+do
+   ./file_generator 5000 $dir/many_files_$i
+   ./file_contaminator $dir/many_files_$i > $dir/true_report_$i.txt
+done
